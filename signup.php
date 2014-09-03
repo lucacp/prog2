2f5ae38db638bbae17e4691640c3f720926ab466
LucasCP <ice0life@gmail.com>
2014-09-03 08:22:58 -0300

    começo do layout

---
+++ signup.php
@@ -0,0 +1,14 @@
+<?php
+	session_start();
+	if (isset($_POST['enviou'])) {
+		$login=$_POST['login'];
+		$pass=$_POST['pass'];
+		if(is_null($login)){
+			$_SESSION['erro1']=1;
+		}
+		if(is_null($pass)){
+			$_SESSION['erro2']=1;
+		}
+		
+	}
+?>
--- t3.php
+++ t3.php
@@ -1,4 +1,54 @@
 <?php
+	session_start();
 ?>
 <html>
+	<head>
+		<title>Registro de eventos gerais</title>
+		<meta charset="UTF-8" />
+		<link rel="stylesheet" type="text/css" href="stilo.css">
+		<script type="text/javascript">
+			function testNull(name,id){
+				if(id==""||id==null)
+					return name."Invalido";
+				else
+					return "";
+			}
+			function testValid(form){
+				var fail;
+				fail = testNull("Usuario ",form.login.value);
+				fail += testNull("Senha ",form.pass.value);
+				if(fail == "")
+					return true;
+				else{
+					alert(fail);
+					return false;
+				}
+
+			}
+		</script>
+	</head>
+	<body>
+		<div id="wrapper">
+			<?php 
+
+				if(!isset($_SESSION['usuario'])){ 
+					echo '<div id="cabeca">
+							<form method="post" action="signup.php" onsubmit="return testValid(this);">
+								<span>Login:</span>
+								<input type="text" name="login" size="8" />
+								<span>Senha:</span>
+								<input type="password" name="pass" size="10" />
+								<input type="submit" name="enviou" value="Enviar" />
+							</form>
+						</div>';
+				}?>
+			<div class="eventos">
+				<div></div>
+				<div></div>
+				<div></div>
+				<div></div>
+				<div></div>
+			</div>
+		</div>
+	</body>
 </html>
