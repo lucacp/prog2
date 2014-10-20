cd7fe3cbcbed129da81eb78b8293b855227e3616
lucacp <ice0life@gmail.com>
2014-10-20 10:01:36 -0200

    atualização bd

--- eventbase.sql
+++ eventbase.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.2.7.1
+-- version 4.0.4.1
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 17, 2014 at 09:13 PM
--- Server version: 5.6.20
--- PHP Version: 5.5.15
+-- Generation Time: Oct 20, 2014 at 02:00 PM
+-- Server version: 5.5.32
+-- PHP Version: 5.4.19
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -19,6 +19,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `eventbase`
 --
+CREATE DATABASE IF NOT EXISTS `eventbase` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `eventbase`;
 
 -- --------------------------------------------------------
 
@@ -27,11 +29,12 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `evento` (
-`eid` int(11) NOT NULL,
+  `eid` int(11) NOT NULL AUTO_INCREMENT,
   `nome` varchar(20) NOT NULL,
   `descr` text,
-  `nivel` int(11) NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+  `nivel` int(11) NOT NULL,
+  PRIMARY KEY (`eid`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
 
 --
 -- Dumping data for table `evento`
@@ -40,7 +43,8 @@ CREATE TABLE IF NOT EXISTS `evento` (
 INSERT INTO `evento` (`eid`, `nome`, `descr`, `nivel`) VALUES
 (1, 'sakura.gif', 'Evento Teste para começo de sistema', 2),
 (2, 'sakuram.gif', 'Evento teste para começo de sistema', 2),
-(3, 'inou.jpg', '', 2);
+(3, 'inou.jpg', 'Evento teste', 2),
+(4, 'precure.jpg', 'evento teste', 2);
 
 -- --------------------------------------------------------
 
@@ -49,10 +53,12 @@ INSERT INTO `evento` (`eid`, `nome`, `descr`, `nivel`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `usuario` (
-`id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `nome` varchar(30) NOT NULL,
   `senha` varchar(32) NOT NULL,
-  `nivel` int(11) NOT NULL
+  `nivel` int(11) NOT NULL,
+  PRIMARY KEY (`nome`),
+  UNIQUE KEY `id` (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=32 ;
 
 --
@@ -64,36 +70,6 @@ INSERT INTO `usuario` (`id`, `nome`, `senha`, `nivel`) VALUES
 (3, 'ice1life', 'd5f3ab3edc9d9f4b372979eaeddf692a', 1),
 (30, 'icelife', 'd41d8cd98f00b204e9800998ecf8427e', 2);
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `evento`
---
-ALTER TABLE `evento`
- ADD PRIMARY KEY (`eid`);
-
---
--- Indexes for table `usuario`
---
-ALTER TABLE `usuario`
- ADD PRIMARY KEY (`nome`), ADD UNIQUE KEY `id` (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `evento`
---
-ALTER TABLE `evento`
-MODIFY `eid` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
---
--- AUTO_INCREMENT for table `usuario`
---
-ALTER TABLE `usuario`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=32;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
