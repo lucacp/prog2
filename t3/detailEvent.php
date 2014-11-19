1945585b064a3b31f2c7d353855a3d02ceaa7e69
lucacp <ice0life@gmail.com>
2014-11-19 11:15:32 -0200

    mostra o local nos detalhes dos Eventos

--- t3/detailEvent.php
+++ t3/detailEvent.php
@@ -12,7 +12,10 @@
 		$query="SELECT `nome`,`descr`,`date`,`lid` FROM `evento` where `eid`='$ev';";
 		$result=mysql_query($query,$csql);
 		$rows=mysql_fetch_assoc($result);
-
+		$lid=$rows['lid'];
+		$query2="SELECT * FROM `local` where `lid`='$lid';";
+		$result2=mysql_query($query2,$csql);
+		$rowsl=mysql_fetch_assoc($result2);
 		include_once '../dataout.php';
 	}
 	else{
@@ -39,28 +42,47 @@
 <body>
 	<div class="container">
 		<div class="thumbnail">
-			<?php include_once '../localImages.php'; echo '<div class="fill text-center"><img class="fill" src="'.$InLocal.$rows['nome'].'" /></div>';?>
+			<?php include_once '../localImages.php'; 
+			echo '<div class="fill text-center"><img class="fill" src="'.$InLocal.$rows['nome'].'" /></div>';?>
 			<div class="thumbnail fill"><span>Nome do Evento:</span>
-			<?php $nom=$rows['nome'];
-			$no=explode(".", $nom);
-			echo '<h3 class="text-center">';
-			for($i=0;$no[$i]!=end($no);$i++) { 
-				if($i!=0)
-					echo '.';
-				echo $no[$i];
-			};
-			echo'</h3></div>'; ?>
-			<div class="fill thumbnail"><span>Detalhes:</span>
-			<?php echo '<h3 class="text-center">'.$rows['descr'].'</h3></div>'; ?>
-			<div class="thumbnail"><span>Data do Evento:</span>
-			<?php 	$data=$rows['date'];
-			$data2=explode("-", $data);
-			$ano=$data2[0];
-			$mes=$data2[1];
-			$dia=$data2[2];
-			echo '<h3 class="text-center">'.$dia.'/'.$mes.'/'.$ano.'</h3></div>'; ?>
+				<?php $nom=$rows['nome'];
+				$no=explode(".", $nom);
+				echo '<h3 class="text-center">';
+				for($i=0;$no[$i]!=end($no);$i++) { 
+					if($i!=0)
+						echo '.';
+					echo $no[$i];
+				};
+				echo'</h3>'; ?>
 			</div>
-
+			<div class="fill thumbnail"><span>Detalhes:</span>	
+				<?php echo '<h3 class="text-center">'.$rows['descr'].'</h3></div>'; ?>
+				<div class="thumbnail"><span>Data do Evento:</span>
+					<?php 	$data=$rows['date'];
+					$data2=explode("-", $data);
+					$ano=$data2[0];
+					$mes=$data2[1];
+					$dia=$data2[2];
+					echo '<h3 class="text-center">'.$dia.'/'.$mes.'/'.$ano.'</h3>'; ?>
+				</div>
+			</div>
+			<div class="fill thumbnail">
+				<span>Local</span>
+				<?php
+					include_once '../localImages.php';
+					echo '<img class="fill" src="'.$InLocalL.$rowsl['nomei'].'" />';
+					echo '<h3 class="text-center">'
+					.'Nome: '.$rowsl['nomel'].'<br />'
+					.'Rua: '.$rowsl['rua'].'<br />'
+					.'Numero: '.$rowsl['num'].'<br />'
+					.'Bairro: '.$rowsl['bairro'].'<br />'
+					.'CEP: '.$rowsl['cep'].'<br />'
+					.'Capacidade: '.$rowsl['capac'].'<br />'
+					.'Cidade: '.$rowsl['cidade'].'<br />'
+					.'UF: '.$rowsl['estado'].'<br />'.'</h3>';
+					echo '</div>';
+				?>
+			</div>
 			<a href="t3.php">Voltar</a>
 		</div>
 	</div>
